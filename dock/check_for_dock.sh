0a62096f2dbc8dac4e69417f720e493fe363aaae
Andreas Lindhé <andreas@lindhe.io>
2021-01-25 19:11:56 +0100

    Update monitor regex in check_for_dock.sh
    With the new Intel driver configuration I found, the naming is slightly
    different.

--- dock/check_for_dock.sh
+++ dock/check_for_dock.sh
@@ -7,7 +7,7 @@ ip link show eth0 2> /dev/null > /dev/null;
 eth_if=$?
 
 # If a monitor is connected to DP-1-1 (or variants), we are probably docked.
-monitors=$(xrandr --query | grep -E 'DP-[1-2]-[1-2] connected')
+monitors=$(xrandr --query | grep -E 'DP-?[1-2]-[1-2] connected')
 
 if [ "$eth_if" == 0 ] || [ -n "$monitors" ]; then
   echo "Connected to dock"
