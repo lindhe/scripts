caf98abe63d36926231ce8ad6ce252f50b4430a6
Andreas Lindhé <andreas@lindhe.io>
2021-06-28 19:14:22 +0200

    Add git_repo positional argument

--- git/git-get.py
+++ git/git-get.py
@@ -21,8 +21,9 @@ __version__ = "0.1.0"
 description = "Takes a URI to a Git repo and clones it into a specified path."
 
 
-def main(dry_run):
+def main(git_repo: str, dry_run: bool):
     """ Clone the git repo """
+    print(f"{git_repo=}")
     print(f"{dry_run=}")
 
 
@@ -30,6 +31,7 @@ if __name__ == '__main__':
     # Bootstrapping
     p = argparse.ArgumentParser(description=description)
     # Add cli arguments
+    p.add_argument('git_repo', help="The git repo URI to clone.")
     p.add_argument('--dry-run', action='store_true',
                    help="Just print, never modify anything.")
     p.add_argument('-V', '--version', action='version', version=__version__)
@@ -37,6 +39,7 @@ if __name__ == '__main__':
     args = p.parse_args()
     try:
         main(
+            git_repo=args.git_repo,
             dry_run=args.dry_run
         )
     except KeyboardInterrupt:
