7f8cec59a41178ab889e01a45b6ddeaadd11ebf5
Andreas Lindhé <andreas@lindhe.io>
2022-07-21 01:16:30 +0000

    [debug] Refactor verbosity

--- backup/nextcloud-snap/backup-nextcloud-snap.sh
+++ backup/nextcloud-snap/backup-nextcloud-snap.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 # shellcheck disable=2181
 
-if [[ "${VERBOSE:-0}" == "4" ]]; then
+set -euo pipefail
+
+if [[ -z ${VERBOSE+x} ]]; then
+    VERBOSE=0
+fi
+readonly VERBOSE
+
+if [[ "${VERBOSE}" -ge 5 ]]; then
     set -x
 fi
-
-set -euo pipefail
 
 readonly MAX_ARCHIVE_SIZE="${MAX_ARCHIVE_SIZE:-$((1024**4))}"  # Size in KiB
 
@@ -31,47 +36,62 @@ if [[ -z "${1:-}" ]]; then
 fi
 
 readonly TARGET_BACKUP_DIR="${1/%\/}"  # Remove trailing /
-if [[ -n ${VERBOSE+x} ]]; then echo "${TARGET_BACKUP_DIR@A}"; fi
+if [[ ${VERBOSE} -ge 1 ]]; then echo "${TARGET_BACKUP_DIR@A}"; fi
 
-if [[ -n ${VERBOSE+x} ]]; then echo "Backup started …"; fi
+if [[ ${VERBOSE} -ge 1 ]]; then echo "Backup started …"; fi
+
+if [[ ${VERBOSE} -eq 0 ]]; then
+    NC_EXPORT_CMD='nextcloud.export 2> /dev/null'
+else
+    NC_EXPORT_CMD='nextcloud.export'
+fi
+readonly NC_EXPORT_CMD
+
+if [[ ${VERBOSE} -ge 1 ]]; then echo "${NC_EXPORT_CMD@A}"; fi
+
 if [[ -n ${DEBUG+x} ]]; then
+    if [[ ${VERBOSE} -ge 1 ]]; then
+        # shellcheck disable=SC2005
+        echo "$(eval echo "${NC_EXPORT_CMD}")"
+    fi
     readonly NC_EXPORT="Successfully exported /var/snap/nextcloud/common/backups/20220720-194312"
 else
-    if [[ -z ${VERBOSE+x} ]]; then
-        MUTE="2> /dev/null"
-        readonly MUTE
+    if [[ ${VERBOSE} -ge 1 ]]; then
+        # shellcheck disable=SC2005
+        echo "$(eval echo "${NC_EXPORT_CMD}")" # TODO
     fi
-    NC_EXPORT="$(nextcloud.export "${MUTE:-}")"
+    NC_EXPORT=$(eval "${NC_EXPORT_CMD}")
     readonly NC_EXPORT
 fi
-if [[ "${VERBOSE}" == "2" ]]; then echo "${NC_EXPORT@A}"; fi
+
+if [[ ${VERBOSE} -ge 2 ]]; then echo "${NC_EXPORT@A}"; fi
 
 if [[ $? -eq 0 ]]; then
-    if [[ -n ${VERBOSE+x} ]]; then echo "Backup finished with exit code ${?}"; fi
+    if [[ ${VERBOSE} -ge 1 ]]; then echo "Backup finished with exit code ${?}"; fi
 
     SUCCESS_STRING=$(echo "${NC_EXPORT}" | grep "Successfully exported") \
         || fail "Grep unexpectedly could not find substring despite successful export!"
     readonly SUCCESS_STRING
-    if [[ "${VERBOSE}" == "2" ]]; then echo "${SUCCESS_STRING@A}"; fi
+    if [[ ${VERBOSE} -ge 2 ]]; then echo "${SUCCESS_STRING@A}"; fi
 
     NC_BACKUP_PATH="$(echo "${SUCCESS_STRING}" | awk '{ print $(NF) }')"
     readonly NC_BACKUP_PATH
-    if [[ "${VERBOSE}" == "2" ]]; then echo "${NC_BACKUP_PATH@A}"; fi
+    if [[ ${VERBOSE} -ge 2 ]]; then echo "${NC_BACKUP_PATH@A}"; fi
 
     NC_BACKUP_DIR="$(dirname "${NC_BACKUP_PATH}")"
     readonly NC_BACKUP_DIR
-    if [[ "${VERBOSE}" == "2" ]]; then echo "${NC_BACKUP_DIR@A}"; fi
+    if [[ ${VERBOSE} -ge 2 ]]; then echo "${NC_BACKUP_DIR@A}"; fi
 
     BACKUP_NAME="$(basename "${SUCCESS_STRING}")"
     readonly BACKUP_NAME
-    if [[ "${VERBOSE}" == "2" ]]; then echo "${BACKUP_NAME@A}"; fi
+    if [[ ${VERBOSE} -ge 2 ]]; then echo "${BACKUP_NAME@A}"; fi
 
 else
     fail "Backup finished with exit code ${?}" $?
 fi
 
-if [[ -n ${VERBOSE+x} ]]; then echo "Exporting backup …"; fi
-if [[ "${VERBOSE}" == "3" ]]; then
+if [[ ${VERBOSE} -ge 1 ]]; then echo "Exporting backup …"; fi
+if [[ ${VERBOSE} -ge 3 ]]; then
     # shellcheck disable=SC2034
     readonly TAR_VERBOSE="--verbose"
 fi
@@ -89,10 +109,10 @@ fi
 if [[ ! $? -eq 0 ]]; then
     fail "Export failed with exit code ${?}" $?
 fi
-if [[ -n ${VERBOSE+x} ]]; then echo "Export complete!"; fi
+if [[ ${VERBOSE} -ge 1 ]]; then echo "Export complete!"; fi
 
-if [[ -n ${VERBOSE+x} ]]; then echo "Cleaning up directory ${NC_BACKUP_PATH} …"; fi
-if [[ "${VERBOSE}" == "3" ]]; then
+if [[ ${VERBOSE} -ge 1 ]]; then echo "Cleaning up directory ${NC_BACKUP_PATH} …"; fi
+if [[ ${VERBOSE} -ge 3 ]]; then
     # shellcheck disable=SC2034
     readonly RM_VERBOSE="--verbose"
 fi
@@ -104,7 +124,7 @@ if [[ -n ${DEBUG+x} ]]; then
 else
     eval "${CLEANUP_CMD}"
 fi
-if [[ -n ${VERBOSE+x} ]]; then echo "Clean-up complete!"; fi
+if [[ ${VERBOSE} -ge 1 ]]; then echo "Clean-up complete!"; fi
 
 #du -k "${TARGET_BACKUP_DIR}"
 if [[ -z ${DEBUG+x} ]]; then
@@ -114,3 +134,8 @@ if [[ -z ${DEBUG+x} ]]; then
     fi
 fi
 
+
+if [[ ${VERBOSE} -ge 1 ]]; then
+    echo "Backup complete!"
+    echo "${TARGET_BACKUP_DIR}/${BACKUP_NAME}.tar.gz"
+fi
