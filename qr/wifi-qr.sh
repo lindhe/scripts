f47b5de750f23f1974f473f335690843df3abd49
Andreas Lindhé <andreas@lindhe.io>
2022-07-25 21:17:02 +0200

    [qr] Add script to show Wi-Fi connection QR code

---
+++ qr/wifi-qr.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+debug() {
+    echo "${@}" 1>&2
+}
+
+fail() {
+    debug "${1}"
+    exit "${2:-1}"
+}
+
+if [[ $# -ne 2 ]]; then
+    debug ""
+    debug "USAGE:"
+    debug "    ${0} SSID PASSWORD"
+    debug ""
+    exit 0
+fi
+
+readonly SSID="${1}"
+readonly PASSWORD="${2//;/\\;}"  # Sanitize the password from bad characters
+readonly ENC_TYPE=WPA2
+
+echo -n "WIFI:S:${SSID};T:${ENC_TYPE};P:${PASSWORD};;" | qrencode -t utf8 -o -
