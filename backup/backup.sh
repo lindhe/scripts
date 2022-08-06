77701cad5e507eadac3bd48d8e4c298354c3d03c
Andreas Lindhé <andreas@lindhe.io>
2022-08-06 10:59:28 +0200

    Allow failure of updating alive file

--- backup/backup.sh
+++ backup/backup.sh
@@ -18,7 +18,9 @@ readonly MAX_SIZE="${1:+ --max-size ${1}}"
 readonly BACKUP_SCRIPT_DIR='/etc/backup'
 
 # Update alive file
+set +e
 (umask 033; date '+%s' > ${BACKUP_SCRIPT_DIR}/alive)
+set -e
 
 readonly HOST=$(hostname)
 readonly CHARGING=$(acpi --ac-adapter | grep "on-line")
