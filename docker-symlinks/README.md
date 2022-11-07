79cfb3647d45bb767b6472b9cfaf42096dc4b9aa
Andreas Lindhé <andreas@lindhe.io>
2022-11-07 22:51:01 +0100

    [docker-symlinks] Create script

---
+++ docker-symlinks/README.md
@@ -0,0 +1,1 @@
+A script for gathering symlinks to Docker's log files, so it's easier to scan them with OpenTelemetry or whatever.
---
+++ docker-symlinks/symlinker.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+debug() {
+    echo "${@}" 1>&2
+}
+
+fail() {
+    debug "${1}"
+    exit "${2:-1}"
+}
+
+readonly DEFAULT_SUFFIX='-json.log'
+readonly DEFAULT_SRC_DIR='/var/lib/docker/containers'
+
+if [[ $# -lt 1 ]]; then
+    debug ""
+    debug "Symlinks Docker logs to DST_DIR and prints each container's SHA"
+    debug ""
+    debug "USAGE:"
+    debug "    ${0} DST_DIR [SUFFIX] [SRC_DIR]"
+    debug ""
+    debug "DEFAULTS:"
+    debug "    SUFFIX='${DEFAULT_SUFFIX}'"
+    debug "    SRC_DIR='${DEFAULT_SRC_DIR}'"
+    debug ""
+    exit 0
+fi
+
+readonly DST_DIR="${1}"
+readonly SUFFIX="${2:-"${DEFAULT_SUFFIX}"}"
+readonly SRC_DIR="${3:-"${DEFAULT_SRC_DIR}"}"
+
+if [ ! -r "${SRC_DIR}" ]; then
+  fail "ERROR: User do not have read access to ${SRC_DIR}"
+fi
+
+for dir in "${SRC_DIR}"/*; do
+  container="$(basename "${dir}")"
+  echo "${container}"
+  logfile="${dir}/${container}${SUFFIX}"
+  ln -sf "${logfile}" -t "${DST_DIR}"
+done
+
+find "${DST_DIR}" -xtype l -exec rm {} \;
