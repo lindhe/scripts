901dc94d126d8629adfad588c8ea2734aca0c37e
Andreas Lindhé <7773090+lindhe@users.noreply.github.com>
2025-12-31 11:29:18 +0100

    Improve wifi QR code generator

--- qr/wifi-qr.sh
+++ qr/wifi-qr.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# {{{
+
 set -uo pipefail
 
 stderr() {
@@ -19,15 +21,31 @@ if [[ $# -lt 2 ]]; then
     exit 0
 fi
 
+missing_dependencies=false
+declare -r dependencies=(
+  qrencode
+)
+for dep in "${dependencies[@]}"; do
+  if ! command -v "${dep}" &> /dev/null; then
+    stderr "❌ ERROR: Missing dependency ${dep}"
+    missing_dependencies=true
+  fi
+done
+if ${missing_dependencies}; then
+  fail 'Please install the missing dependencies!'
+fi
+
+# }}}
+
 declare -r SSID="${1}"
 declare -r PASSWORD="${2//;/\\;}"  # Sanitize the password from bad characters
 declare -r ENC_TYPE=WPA
 
 
 if [[ $# -eq 3 ]]; then
-    declare -r QR_CMD="qrencode --output=${3}"
+  declare -ar FLAGS=("--output=${3}")
 else
-    declare -r QR_CMD='qrencode -t utf8 -o -'
+  declare -ar FLAGS=('-t' 'utf8' '-o' '-')
 fi
 
-echo -n "WIFI:S:${SSID};T:${ENC_TYPE};P:${PASSWORD};;" | ${QR_CMD}
+echo -n "WIFI:S:${SSID};T:${ENC_TYPE};P:${PASSWORD};;" | qrencode "${FLAGS[@]}"
